b4ff2d18fb52bef660fc0f7ea6d043544a938b79
Ahmad Mustofa <mustofaahmad@poltera.ac.id>
2026-02-19 10:05:56 +0700

    change favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'ICMQTT')</title>
