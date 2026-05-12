0cfbbe41757409b0ac368c2d7cfaa8f4710c8eac
Ahmad Mustofa <mustofaahmad@poltera.ac.id>
2026-05-13 01:23:43 +0700

    fixing landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,8 @@
 
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/css/welcome.css'])
+    @else
+        <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
     @endif
 </head>
 <body class="landing-page">
